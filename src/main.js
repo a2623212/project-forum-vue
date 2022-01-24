9e8ecf11b10fc8ba4a0c112192558cd847e46a96
a2623212@gmail.com <A5GjZK7w>
2022-01-24 17:15:14 +0800

    feat: install vue router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app') //指稱我現在要用 app.vue 產生內容，產生之後請幫我灌到 #app (HTML) 中
