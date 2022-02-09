8f523c75302681128414af3716555a324f2c2573
a2623212@gmail.com <A5GjZK7w>
2022-02-09 11:02:54 +0800

    feat: edit userFavoriteRestaurant part of the single profile page

---
+++ src/components/UserFavoritedRestaurantsCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <strong>{{ favoritedRestaurants.length }}</strong> 收藏的餐廳
+    </div>
+    <div class="card-body">
+      <router-link
+        v-for="favoritedRestaurant in favoritedRestaurants"
+        :key="favoritedRestaurant.id"
+        :to="{ name: 'restaurant', params: { id: favoritedRestaurant.id } }"
+      >
+        <img
+          :src="favoritedRestaurant.image | emptyImage"
+          width="60"
+          height="60"
+          class="mr-1 mb-1"
+        />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { emptyImageFilter } from "./../utils/mixins";
+
+export default {
+  name: "userFavoritedRestaurantsCard",
+  mixins: [emptyImageFilter],
+  props: {
+    favoritedRestaurants: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
