6e09978d7de4c3a7d5f1b7a403d37c6eb2a2ceb5
a2623212@gmail.com <A5GjZK7w>
2022-02-20 00:27:00 +0800

    feat: add vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app') //指稱我現在要用 app.vue 產生內容，產生之後請幫我灌到 #app (HTML) 中
